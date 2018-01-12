49379a5cfbafd12e10ea1eacb705dd23f48fa7b5
Bence Ivady <bence.ivady@emarsys.com>
2018-01-12 16:05:53 +0100

    hello

---
+++ test/dummyTest.php
@@ -0,0 +1,12 @@
+<?php
+require './vendor/autoload.php';
+
+class AppBuilderTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @test
+     */
+    public function TestOne() {
+        $this->assertTrue(true);        
+    }
+}
